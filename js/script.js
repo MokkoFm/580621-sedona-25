4a4b2e3fb023c86c2352f6d573716f253099cdde
MokkoFm <mokkofmpoetry@gmail.com>
2019-06-11 02:13:22 +0300

    Автопрефиксер CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".activate-form-btn");
 var showForm = document.querySelector(".search-form");
 
-link.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    showForm.classList.toggle("no-form");
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  showForm.classList.toggle("no-form");
 });
