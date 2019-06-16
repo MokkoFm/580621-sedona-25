7e6beef37800759136ac8b53e124b4fefe741fc5
MokkoFm <mokkofmpoetry@gmail.com>
2019-06-17 01:36:24 +0300

    Финальные правки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 var link = document.querySelector(".activate-form-btn");
 var showForm = document.querySelector(".search-form");
+var form = showForm.querySelector(".appointment-form");
+var arrivalDate = showForm.querySelector("[name=arrival]");
+var departureDate = showForm.querySelector("[name=departure]");
+var adult = showForm.querySelector("[name=adults]");
+var child = showForm.querySelector("[name=children]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   showForm.classList.toggle("no-form");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !adult.value || !child.value) {
+  evt.preventDefault();
+  showForm.classList.remove("modal-error");
+  showForm.offsetWidth = showForm.offsetWidth;
+  showForm.classList.add("modal-error");
+  console.log("Пожалуйста, заполните все поля формы");
+  }
 });
